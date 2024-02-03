44a006831eda6f4ed91e375b954e5b9fc9df0ca8
esianyo <esianicd@gmail.com>
2024-02-03 22:15:33 +0000

    Added / to routes in the auth file

--- website/auth.py
+++ website/auth.py
@@ -4,11 +4,11 @@ auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def login():
-    return "<p>login</p>"
+    return "<p>Log In</p>"
 
 @auth.route('/logout')
 def logout():
-    return "<p>logout</p>"
+    return "<p>Log Out</p>"
 
 @auth.route('/sign-up')
 def sign_up():
